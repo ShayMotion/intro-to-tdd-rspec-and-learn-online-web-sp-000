4be4722f541de2e982bc9d2b8dc07419f4efcd8f
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-07-17 01:31:49 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../current_age_for_birth_year.rb'
 
+def current_age_for_birth_year(birth_year)
+2003 - birth_year
+end
+
 age_of_person = current_age_for_birth_year(1984)
 
 describe "current_age_for_birth_year method" do
@@ -8,8 +12,4 @@ describe "current_age_for_birth_year method" do
 
     expect(age_of_person).to eq(19)
   end
-end
-
-def current_age_for_birth_year(birth_year)
-2003 - birth_year
 end
